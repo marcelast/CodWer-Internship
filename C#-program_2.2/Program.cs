137c23b96e34b32e6aa39c6d905c361c805db430
marcelast <71786654+marcelast@users.noreply.github.com>
2023-09-14 16:29:18 +0300

    2.2 OOP basics

---
+++ C#-program_2.2/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using static MyApplication.Animal;
+
+namespace MyApplication
+{
+
+    class Program
+    {
+        public static Animal CreeazaAnimal(TipAnimal tip, string nume, decimal greutate, Dimensiune dimensiune, decimal viteza)
+            {
+            switch (tip)
+            {
+                case TipAnimal.Lup: 
+                    return new Carnivor(nume, greutate, dimensiune, viteza);
+                case TipAnimal.Urs:
+                    return new Omnivor(nume, greutate, dimensiune, viteza);
+                case TipAnimal.Oaie: 
+                    return new Carnivor(nume, greutate, dimensiune, viteza);
+                case TipAnimal.Veverita: 
+                    return new Omnivor(nume, greutate, dimensiune, viteza);
+                case TipAnimal.Pisica: 
+                    return new Carnivor(nume, greutate, dimensiune, viteza);
+                case TipAnimal.Vaca:
+                    return new Erbivor(nume, greutate, dimensiune, viteza);
+                default:
+                    throw new ArgumentException("Tipul de animal dat nu este valid.");
+
+            }
+            }
+        public static void Main(string[] args)
+        {
+            Carnivor lup = new Carnivor("Lupul", 30.5m, new Dimensiune { lungime = 0.7m, latime = 0.4m, inaltime = 0.8m }, 20m);
+            Erbivor oaie = new Erbivor("Oaia", 15.2m, new Dimensiune { lungime = 0.9m, latime = 0.5m, inaltime = 0.6m }, 15m);
+            Omnivor urs = new Omnivor("Ursul", 150m, new Dimensiune { lungime = 2m, latime = 1.2m, inaltime = 1.8m }, 10m);
+            Planta salata = new Planta(0.5m, 0.01m);
+            Carne sunca = new Carne(1, 0.05m);
+
+            //Hranim animalele
+            lup.Mananca(sunca);
+            lup.Mananca(sunca);
+            oaie.Mananca(salata);
+            oaie.Mananca(salata); 
+            oaie.Mananca(salata);
+            urs.Mananca(sunca);
+            urs.Mananca(salata);
+            urs.Mananca(salata);
+
+            //Punem animalele sa alerge
+            lup.ALearga(200);
+            oaie.ALearga(200);
+            urs.ALearga(200);
+
+            //nr de animale create
+            Console.WriteLine($"Numar total animale instantiate: {Animal.contor}");
+
+            //verificare dupa tipul de mancare
+            lup.Mananca(salata);
+
+            //afisare obiect prin functia suprascrisa ToString()
+            Erbivor capra = new Erbivor("Capra", 15m, new Dimensiune { lungime = 0.6m, latime = 0.5m, inaltime = 0.6m }, 10m);
+            Console.WriteLine(capra);
+
+            //Creare animal prin fucntia statica CreeazaAnimal(...)
+            Animal vaca = CreeazaAnimal(TipAnimal.Vaca, "Vaca", 120m, new Dimensiune { inaltime = 1.50m, latime = 0.9m, lungime = 0.9m }, 5m);
+            Console.WriteLine(vaca);
+
+            List<Animal> animaleList = new List<Animal>();
+            Random rand = new Random();
+            for (int i = 1; i<=10; i++)
+            {
+                int nrAnimal = rand.Next(6);
+                Animal animal = CreeazaAnimal((TipAnimal)nrAnimal, "Animal "+i,
+                                                rand.Next(1, 200),
+                                                new Dimensiune
+                                                {
+                                                    lungime = rand.Next(1, 11) / 10m,
+                                                    latime = rand.Next(1, 11) / 10m,
+                                                    inaltime = rand.Next(1, 11) / 10m
+                                                },
+                                                rand.Next(1, 21));
+                animaleList.Add(animal);
+
+            }
+          
+            int manancaCarne = 0, manancaPlante = 0;
+            foreach(Animal a in animaleList)
+            {
+                if(a is Carnivor)
+                {
+                    manancaCarne++;
+                    a.Mananca(sunca);
+                }
+                else if(a is Erbivor)
+                {
+                    manancaPlante++;
+                    a.Mananca(salata);
+                }
+                else
+                {
+                    manancaPlante++;
+                    manancaCarne++;
+                    a.Mananca(sunca);
+                    a.Mananca(salata);
+                }
+               
+            }
+            Console.WriteLine("\n\nStatistici");
+            Console.WriteLine($"{animaleList.Count()} animale mananca Mancare.");
+            Console.WriteLine($"{manancaCarne} animale mananca Carne.");
+            Console.WriteLine($"{manancaPlante} animale mananca Plante.");
+        }
+    }
+}
---
+++ C#-program_2.2/Program2.cs
@@ -0,0 +1,161 @@
+﻿
+namespace MyApplication
+
+{
+    abstract class Animal 
+    {
+        public static int contor;
+        public string nume { get; set; }
+        public decimal greutate { get; private set; }
+        public struct Dimensiune
+        {
+            public decimal lungime;
+            public decimal latime;
+            public decimal inaltime;
+        }
+        public Dimensiune dimensiune { get; private set; }
+        public decimal viteza { get; private set; }
+        protected List<Mancare> stomac = new List<Mancare>();
+        public enum TipAnimal {Lup, Urs, Oaie, Veverita, Pisica, Vaca}
+
+        //constructor
+        public Animal(string nume, decimal greutate, Dimensiune dimensiune, decimal viteza)
+        {
+            this.nume = nume;
+            this.greutate = greutate;
+            this.dimensiune = dimensiune;
+            this.viteza = viteza;
+            contor++;
+        }
+         public void Mananca(Mancare m)
+         {
+            if (((this is Carnivor && m is Carne) ||
+                (this is Erbivor && m is Planta) ||
+                (this is Omnivor && (m is Carne || m is Planta))) && (m.greutate <= this.greutate / 8))
+            {
+                 stomac.Add(m);
+                 Console.WriteLine($"{nume} mananca {m.GetType().Name.ToLower()}");
+            }
+            else
+            {
+                 Console.WriteLine($"{nume} nu poate manca {m.GetType().Name.ToLower()}!");
+            }
+         }
+       
+        public abstract double Energie();
+        public void ALearga(decimal distanta)
+        {
+            decimal timp;
+            timp = distanta / (viteza / (decimal)Energie());
+            timp = Math.Round(timp, 2);
+            Console.WriteLine($"{nume} parcurge {distanta} m in {timp} secunde");
+        }
+        public override string ToString()
+        {
+            Console.WriteLine("\n* * * * * * * * * * * * * * ");
+            return $"Tip animal: {this.GetType().Name}\n" +
+                $"Nume: {this.nume}\n" +
+                $"Greutate: {this.greutate} kg\n" +
+                $"Dimensiuni: {this.dimensiune.lungime} x {this.dimensiune.latime} x {this.dimensiune.inaltime}" +
+                $"\nViteza: {this.viteza} m/s";
+        }
+    }
+    class Carnivor: Animal
+    {
+        public Carnivor(string nume, decimal greutate, Dimensiune dimensiune, decimal viteza) 
+            : base(nume, greutate, dimensiune, viteza) { }
+    
+        public override double Energie()
+        {
+            decimal greutateMancare = 0;
+            decimal energieMancare = 0;
+            foreach (Mancare m in stomac)
+            {
+                greutateMancare += m.greutate;
+                energieMancare += m.energie;
+            }
+            decimal mediaGreutateMancare = greutateMancare / stomac.Count;
+            double nivelEnergie = 0.2 - (double)(1.0 / 5.0) * (double)mediaGreutateMancare + (double)energieMancare;
+            return nivelEnergie;
+        }
+    }
+    class Erbivor: Animal
+    {
+    public Erbivor(string nume, decimal greutate, Dimensiune dimensiune, decimal viteza) 
+        : base(nume, greutate, dimensiune, viteza) { }
+
+    public override double Energie()
+        {
+            decimal greutateMancare = 0;
+            decimal energieMancare = 0;
+            foreach (var m in stomac)
+            {
+                greutateMancare += m.greutate;
+                energieMancare += m.energie;
+            }
+            decimal mediaGreutateMancare = greutateMancare / stomac.Count;
+            double nivelEnergie = 0.5 + (double)(1.0 / 3.0) * (double)mediaGreutateMancare + (double)energieMancare;
+            return nivelEnergie;
+        }
+
+    }
+    class Omnivor : Animal  
+    {
+    public Omnivor(string nume, decimal greutate, Dimensiune dimensiune, decimal viteza) 
+        : base(nume, greutate, dimensiune, viteza) { }
+    public override double Energie()
+        {
+            double mediaGreutateMancare = 0;
+            double sumaEnergieMancare = 0;
+
+            foreach (Mancare m in stomac)
+            {
+                mediaGreutateMancare += (double)m.greutate;
+                sumaEnergieMancare += (double)m.energie;
+            }
+
+            if (stomac.Count > 0)
+            {
+                mediaGreutateMancare /= stomac.Count;
+
+                double coefGreutate = 0;
+
+                foreach (Mancare m in stomac)
+                {
+                    if (m is Planta)
+                    {
+                        coefGreutate += 0.5;
+                    }
+                    else if (m is Carne)
+                    {
+                        coefGreutate -= 0.5;
+                    }
+                }
+
+                return 0.35 + coefGreutate * mediaGreutateMancare + sumaEnergieMancare;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+    abstract class Mancare
+    {
+        public decimal greutate { get; set; }
+        public decimal energie { get; set; }
+        public Mancare(decimal greutate, decimal energie)
+        {
+            this.greutate = greutate;
+            this.energie = energie;
+        }
+    }
+    class Carne : Mancare
+    {
+        public Carne(decimal greutate, decimal energie) : base(greutate, energie) { }
+    }
+    class Planta: Mancare
+    {
+        public Planta(decimal greutate, decimal energie) : base(greutate, energie) { }
+    }
+}
