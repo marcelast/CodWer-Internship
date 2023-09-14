2e0d9a93f1cf2279b595f743dc452f495cd1c7ba
marcelast <71786654+marcelast@users.noreply.github.com>
2023-09-14 16:35:06 +0300

    2.3 First program arhitecture

---
+++ Ex-2.3/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Ex_2._3
+{
+    class Program
+    {
+
+        delegate bool MyDelegate(int x, int y, int z);
+        static void Main(string[] args)
+        {
+            CoffeeMachine coffee= new CoffeeMachine();
+            MyDelegate check = coffee.VerificaResurse;
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.Clear();
+                Console.WriteLine("Selectati o optiune: ");
+                Console.WriteLine("1. Prepara espresso");
+                Console.WriteLine("2. Prepara cappuccino");
+                Console.WriteLine("3. Curata aparat");
+                Console.WriteLine("4. Verifica nivelul de resurse");
+                Console.WriteLine("0. Exit");
+
+                if(coffee.VerificaResurse())
+                {
+                    Console.Clear();
+                    Console.WriteLine("Adauga resurse:");
+                    Console.WriteLine("Nivelul de apa este = {0} -> Tasteaza 5 pentru a adauga apa", coffee.nivelApa);
+                    Console.WriteLine("Nivelul de lapte este = {0} -> Tasteaza 6 pentru a adauga lapte", coffee.nivelLapte);
+                    Console.WriteLine("Nivelul de cafea este = {0} -> Tasteaza 7 pentru a adauga cafea", coffee.nivelCafea);
+                }
+                if(coffee.nivelZatCafea == 5)
+                {
+                    Console.Clear();
+                    Console.WriteLine("E nevoie sa curatati aparatul pentru a continua");
+                    Console.WriteLine("Tasteaza 3 pentru a curati aparatul.");
+                }
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 0:
+                        exit = true;
+                        break;
+                    case 1:
+                        if (check(1, 1, 0))
+                        {
+                            coffee.PreparaEspresso();
+                            Console.WriteLine("Espresso preparat!");
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Resurse insuficiente pentru a prepara espresso.\n Adauga Resursele necesare.");
+                        }
+                        break;
+                    case 2:
+                        if (check(1, 1, 1))
+                        {
+                            coffee.PreparaCappuccino();
+                            Console.WriteLine("Cappuccino preparat!");
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Resurse insuficiente pentru a prepara cappuccino.\n Adauga Resursele necesare.");
+                        }
+                        break;
+                    case 3:
+                        coffee.CurataAparat();
+                        Console.Clear();
+                        Console.WriteLine("Aparatul de cafea a fost curatat.");
+                        break;
+                    case 4:
+                        coffee.AfiseazaResurse();
+                        break;
+                    case 5: Console.WriteLine("Introdu nivelul de apa:");
+                        int apa = Convert.ToInt32(Console.ReadLine());
+                        coffee.nivelApa = apa;
+                        break;
+                    case 6:
+                        Console.WriteLine("Introdu nivelul de lapte:");
+                        int lapte = Convert.ToInt32(Console.ReadLine());
+                        coffee.nivelLapte = lapte;
+                        break;
+                    case 7:
+                        Console.WriteLine("Introdu nivelul de cafea:");
+                        int cafea = Convert.ToInt32(Console.ReadLine());
+                        coffee.nivelCafea = cafea;
+                        break;
+                    default:
+                        Console.WriteLine("Optiune invalida");
+                        break;
+
+                }
+                Console.WriteLine("Apasati o tasta pentru a continua...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Ex-2.3/coffeeMachine.cs
@@ -0,0 +1,45 @@
+﻿class CoffeeMachine
+{
+    public int nivelCafea = 2;
+    public int nivelApa = 2;
+    public int nivelZatCafea = 3;
+    public int nivelLapte = 2;
+    public bool cup = true;
+
+    public bool VerificaResurse(int cafeaNecesara, int apaNecesara, int lapteNecesar)
+    {
+        return nivelCafea >= cafeaNecesara && nivelApa >= apaNecesara && nivelLapte >= lapteNecesar;
+    }
+    public bool VerificaResurse()
+    {
+        return nivelCafea <= 0 || nivelApa <= 0 || nivelLapte <= 0;
+    }
+
+    public void PreparaEspresso()
+    {
+        nivelCafea -= 1;
+        nivelApa -= 1;
+        nivelZatCafea += 1;
+    }
+
+    public void PreparaCappuccino()
+    {
+        nivelCafea -= 1;
+        nivelApa -= 1;
+        nivelZatCafea += 1;
+        nivelLapte -= 1;
+    }
+
+    public void CurataAparat()
+    {
+        nivelZatCafea = 0;
+    }
+
+    public void AfiseazaResurse()
+    {
+        Console.WriteLine("- nivelul de cafea: " + nivelCafea);
+        Console.WriteLine("- nivelul de apa: " + nivelApa);
+        Console.WriteLine("- nivelul de lapte: " + nivelLapte);
+        Console.WriteLine("- nivelul de zat de cafea: " + nivelZatCafea);
+    }
+}
