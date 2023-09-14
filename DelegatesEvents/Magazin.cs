1c2472419cc66b6f6d438e596a947ca4c5cb70a2
marcelast <71786654+marcelast@users.noreply.github.com>
2023-09-14 16:36:26 +0300

    2.4 Delegates & events

---
+++ DelegatesEvents/Magazin.cs
@@ -0,0 +1,173 @@
+﻿
+using System.Net.Http.Headers;
+
+namespace DelegatesEvents
+{
+    public delegate void MyDelegate<T, T2>(T neww, T old, T2 obj);
+    class Reducere
+    {
+        public string Nume;
+        public DateTime Data;
+        public Reducere (string nume, DateTime data)
+        {
+            Nume = nume;
+            Data = data; 
+        }
+        public Action<Produs> Aplica => (produs) =>
+        {
+            produs.Pret.Valoarea -= produs.Pret.Valoarea * 0.1m;
+        };
+
+    }
+      class Producator
+    {
+        public string Nume;
+        public List<Reducere> Reduceri;
+
+        public Producator(string nume, List<Reducere> reduceri)
+        {
+            Nume = nume;
+            Reduceri = reduceri;
+        }
+    }
+    class Produs
+    {
+        public Guid Id;
+        public String Nume;
+        private Pret pret;
+        public Pret Pret
+        {
+            get { return pret; }
+            set
+            {
+                decimal oldPrice = pret.Valoarea;
+                pret = value;
+                decimal newPrice = pret.Valoarea;
+                OnPriceChanged(newPrice, oldPrice, this);
+            }
+        }
+        private int stoc;
+        public int Stoc
+        {
+            get { return stoc; }
+            set
+            {
+                int oldStock = stoc;
+                stoc = value;
+                OnStockChanged(stoc, oldStock, this);
+            }
+        }
+        // adaugam evenimente
+        public event MyDelegate<int, Produs> StockChanged;
+        protected virtual void OnStockChanged(int newStock, int oldStock, Produs obj)
+        {
+            StockChanged?.Invoke(newStock, oldStock, obj);
+        }
+       
+        public event MyDelegate<decimal, Produs> PriceChanged;
+
+        protected virtual void OnPriceChanged(decimal newPrice, decimal oldPrice, Produs obj)
+        {
+            PriceChanged?.Invoke(newPrice, oldPrice, obj);
+        }
+
+        public Producator Producator;
+
+        public Produs(Guid id, string nume, Pret pret, int stoc, Producator producator)
+        {
+            Id = id;
+            Nume = nume;
+            this.pret = pret;
+            this.stoc = stoc;
+            Producator = producator;
+        }
+    
+    }
+ 
+    class Pret
+    {
+        public static Dictionary<Moneda, decimal> Curs = new Dictionary<Moneda, decimal>();
+        public decimal Valoarea;
+        public Moneda Moneda;
+
+        public Pret(decimal valoarea, Moneda moneda)
+        {
+            Valoarea = valoarea;
+            Moneda = moneda;
+        }
+    
+        public decimal ValoareCurs(Moneda moneda)
+        {
+            if (Curs.ContainsKey(moneda))
+            {
+                Console.WriteLine("Cursul valutar pentru {0} este = {1}", moneda, Curs[moneda]);
+                return Curs[moneda];
+            }
+            else
+            {
+                throw new ArgumentException("Nu s-a gasit cursul pentru aceasta moneda");
+            }
+        }
+       
+    }
+    public enum Moneda
+    {
+        LEU, EUR, USD
+    }
+    class Catalog
+    {
+        public List<Produs> Produse;
+        public DateTime? PerioadaStart;
+        public DateTime? PerioadaStop;
+        public List<Reducere> Reduceri;
+
+        public Catalog(List<Produs> produs, DateTime? start, DateTime? stop, List<Reducere> reduceri) 
+        {  
+            Produse = produs; 
+            PerioadaStart = start;  
+            PerioadaStop = stop;
+            Reduceri = reduceri;
+        
+        }
+        public void AboneazaClient(Client client)
+        {
+            foreach (var produs in Produse)
+            {
+                if (client.ProduseFavorite.Contains(produs.Id))
+                {
+                    produs.PriceChanged += (newPrice, oldPrice, obj) =>
+                    {
+                        string mesaj = $"Pret-ul produsului {obj.Nume} s-a schimbat de la {oldPrice} {obj.Pret.Moneda} la {newPrice} {obj.Pret.Moneda}.";
+                        client.Notifica(mesaj);
+                    };
+                }
+            }
+        }
+
+    }
+    class Client
+    {
+        private string[] inbox = new string[10];
+        public string email;
+        public Moneda moneda;
+        public List<object> ProduseFavorite;
+
+
+        public bool Notifica(string mesaj)
+        {
+            if(mesaj.Length > 60)
+            {
+                return false;
+            }
+            else if (inbox.Length > 10)
+            {
+                throw new OutOfMemoryException("Inbox-ul este plin.");
+            }
+            else
+            {
+                inbox.Append(mesaj);
+                return true;
+            }
+        }
+    }
+}
---
+++ DelegatesEvents/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace DelegatesEvents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // a. Lista de producatori
+            List<Producator> producatori = new List<Producator>();
+
+            Producator prod1 = new Producator("Apple", new List<Reducere>
+           {
+               new Reducere ("reducere de vara", new DateTime(2023, 7, 10)),
+               new Reducere ("reducere de tomana", new DateTime(2023, 9, 20))
+           });
+            Producator prod2 = new Producator("Samsung", new List<Reducere>
+           {
+               new Reducere ("reducere de vara", new DateTime(2023, 8, 10)),
+               new Reducere ("reducere de tomana", new DateTime(2023, 10, 20))
+           });
+            Producator prod3 = new Producator("Nokia", new List<Reducere>
+           {
+               new Reducere ("reducere de vara", new DateTime(2023, 6, 10)),
+               new Reducere ("reducere de tomana", new DateTime(2023, 11, 1))
+           });
+            producatori.Add(prod1);
+            producatori.Add(prod2);
+            producatori.Add(prod3);
+
+            // b. instantiere catalog
+            List<Produs> produse = new List<Produs>();
+            Produs produs1 = new Produs(Guid.NewGuid(),"Iphone 11", new Pret(15000, Moneda.LEU), 20, prod1);
+            Produs produs2 = new Produs(Guid.NewGuid(), "Samsung Galaxy 5", new Pret(10000, Moneda.LEU), 10, prod2);
+            Produs produs3 = new Produs(Guid.NewGuid(), "Nokia 6300", new Pret(3000, Moneda.LEU), 0, prod3);
+            produse.Add(produs1);
+            produse.Add(produs2);
+            produse.Add(produs3);
+
+            Catalog catalog1 = new Catalog(produse, DateTime.Now.AddDays(5), null, new List<Reducere>
+           {
+               new Reducere ("reducere de vara", new DateTime(2023, 7, 10)),
+               new Reducere ("reducere de tomana", new DateTime(2023, 9, 20))
+           });
+
+            // c. stabilire curs valutar
+            Pret.Curs[Moneda.LEU] = 4.05m;
+            Pret.Curs[Moneda.EUR] = 19.99m;
+            Pret.Curs[Moneda.USD] = 18.35m;
+
+            // d. Instantiere lista clienti
+
+            List<Client> clienti = new List<Client>
+    {
+        new Client
+        {
+            email = "marcela@email.com",
+            moneda = Moneda.LEU,
+            ProduseFavorite = new List<object> { produs1.Id} 
+        },
+        new Client
+        {
+            email = "claudiu@email.com",
+            moneda = Moneda.EUR,
+            ProduseFavorite = new List<object> { produs1.Id, produs2.Id}
+        },
+        new Client
+        {
+            email = "marius@email.com",
+            moneda = Moneda.USD,
+            ProduseFavorite = new List<object> { produs1.Id, produs3.Id, produs2.Id}
+        }
+    };
+            //verificare evenimente
+            produs1.StockChanged += NotifyStock;
+            produs1.PriceChanged += NotifyPrice;
+           // produs1.Stoc = 25;
+           // produs1.Pret = new Pret(20000, Moneda.LEU);
+
+            //abonarea la catlog
+            Client client1 = new Client
+            {
+                email = "andrei@email.com",
+                moneda = Moneda.USD,
+                ProduseFavorite = new List<object> { produs1.Id, produs3.Id, produs2.Id }
+            };
+            catalog1.AboneazaClient(client1);
+
+
+
+
+        }
+        public static void NotifyPrice(decimal newp, decimal oldp, Produs obj)
+        {
+            Console.WriteLine($"Pretul pentru produsul: {obj.Nume}");
+            Console.WriteLine($"Pretul nou este {newp} \nPretul vechi este {oldp}");
+            Console.WriteLine("- - - - - - - - - - - - - - - - - -");
+            Console.ReadLine();
+        }
+        public static void NotifyStock(int news, int olds, Produs obj)
+        {
+            Console.WriteLine($"Stocul pentru produsul: {obj.Nume}");
+            Console.WriteLine($"Stocul noue este {news} \nStocul vechi este {olds}");
+            Console.WriteLine("- - - - - - - - - - - - - - - - - -");
+            Console.ReadLine();
+        }
+    }
+}
